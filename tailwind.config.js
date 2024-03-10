1602467b43f8c66f101dab462bfe4346573d06c5
panqvela18 <irakli.pankvelashvili@gmail.com>
2024-03-10 21:00:12 +0400

    add responsive and dropdown burger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      md: { max: "1024px" },
+
+      sm: { max: "640px" },
+    },
   },
   plugins: [],
 };
